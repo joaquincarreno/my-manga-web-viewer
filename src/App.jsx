f2e9020b0527d6dab2a7816855d5bb791895089f
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-04 00:05:51 -0400

    se ve una imagen!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import portada from "./mangas/Inside Mari/V01/00.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>holis</div>
-    </>
+    <div className="img-container">
+      <img src={portada} className="center-fit"></img>
+    </div>
   );
 }
 
