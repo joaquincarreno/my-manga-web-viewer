c0ded127e01248fdf957af6dc9e9387863dde6e0
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-04 00:46:05 -0400

    api comenzada que muestra los posibles mangas

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import pathlib
+from os import listdir
 
 # Create your views here.
+@api_view(['GET'])
+def getAvailableMangas(request):
+    path = pathlib.Path('./api/mangas')
+    files = listdir(path)
+    return Response(data=files)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-52_8u$z0dn4ejo6el-1o9_+q+hl70zwg7#9po^6($qti&mw_!)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '192.168.100.*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
