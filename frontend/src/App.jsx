f954f13e0e7c6377aef28187d5e3e786ed80cc14
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-05 00:28:37 -0400

    feat: navbar con selects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Navbar from "./modules/Navbar";
 
 import axios from "axios";
 
--- frontend/src/modules/Navbar.jsx
+++ frontend/src/modules/Navbar.jsx
@@ -8,6 +8,22 @@ function Navbar({
   page = 1,
   pageSetter = () => {},
 }) {
+  const createHandler = (varName, setter) => {
+    return (event) => {
+      const value = event.target.value;
+      console.log(value);
+      setter(value);
+      console.log("selected " + varName + ": " + value);
+    };
+  };
+  const createHandlerNumeric = (varName, offset, setter) => {
+    return (event) => {
+      const value = Number(event.target.value);
+      console.log(value);
+      setter(value + offset);
+      console.log("selected " + varName + ": " + value);
+    };
+  };
   const createSelector = (currentValue, options, handler) => {
     return (
       <select value={currentValue} onChange={handler}>
@@ -19,16 +35,39 @@ function Navbar({
       </select>
     );
   };
+
+  // console.log(mangaInfo.pages_per_volume[volume - 1]);
+  // console.log(volume);
+  // console.log(mangaInfo.pages_per_volume[volume]);
+  const page_num = mangaInfo.pages_per_volume[volume];
+  // console.log(typeof page_num);
+  const page_array = [...Array(page_num + 1).keys()].slice(1, page_num + 1);
+  // console.log(page);
+  // console.log(page_num);
+
   return (
     <div>
       {/* manga selector */}
-      {createSelector(selectedManga, availableMangas, (target) =>
-        console.log(target.value)
+      {createSelector(
+        selectedManga,
+        availableMangas,
+        createHandler("manga", mangaSetter, [])
       )}
       {/* volume selector */}
-      <select></select>
+      {createSelector(
+        volume,
+        [...Array(mangaInfo.n_volumes + 1).keys()].slice(
+          1,
+          mangaInfo.n_volumes + 1
+        ),
+        createHandlerNumeric("volume", -1, volumeSetter, [])
+      )}
       {/* page selector */}
-      <select></select>
+      {createSelector(
+        page,
+        page_array,
+        createHandlerNumeric("page", -1, pageSetter, [])
+      )}
     </div>
   );
 }
