89ed469460b0e4947d08064f06273ed6912f88c0
Joaquín Carreño <joaquin.carreno.h@ug.uchile.cl>
2024-07-04 22:27:25 -0400

    feat: retrieve image and display it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
-// import listFiles from "list-files";
-import portada from "./mangas/Inside Mari/V01/00.png";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+import axios from "axios";
+
+const BACKEND_IP = "http://0.0.0.0:8000/";
+// const BACKEND_IP = "http://192.172.100.17:8000/";
+const MANGAS_API = BACKEND_IP + "availableMangas/";
+const VOLUMES_API = BACKEND_IP + "availableVolumes/";
+const PAGES_API = BACKEND_IP + "getPage/";
+
 function App() {
-  const [count, setCount] = useState(0);
-  // console.log(listFiles());
+  const [image, setImage] = useState([]);
+  const [imagesReady, setImagesReady] = useState(false);
+
+  const [manga, setManga] = useState("Inside Mari");
+  const [volume, setVolume] = useState(1);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    axios
+      .get(PAGES_API + manga + "/" + volume + "/" + page)
+      .then((response) => {
+        setImage(response.data.page);
+      })
+  }, [page, volume]);
   return (
     <div className="img-container">
-      <img src={portada} className="center-fit"></img>
+      {imagesReady ? (
+        "hola"
+      ) : (
+        <img
+          src={`data:image/png;base64, ${image}`}
+          className="center-fit"
+          // onClick={processClick}
+        ></img>
+      )}
     </div>
   );
 }
